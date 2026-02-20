043ae06f8cb087eded78998cb3db624e37e97952
astrNVlik <wertybrothers9@gmail.com>
2026-02-20 10:54:31 +0700

    Updated - Phone Number and division_id for user model

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -72,8 +72,16 @@ export const getUserById = async (req: Request, res: Response) => {
  * CREATE USER (Admin Only)
  */
 export const createUser = async (req: Request, res: Response) => {
-  const { name, email, password, organization_id, department_id, role_id } =
-    req.body;
+  const { 
+    name, 
+    email, 
+    password, 
+    phone_number,
+    organization_id, 
+    department_id, 
+    division_id,
+    role_id 
+  } = req.body;
 
   try {
     const existing = await prisma.user.findUnique({
@@ -90,9 +98,11 @@ export const createUser = async (req: Request, res: Response) => {
       data: {
         name,
         email,
+        phone_number,
         password: hashedPassword,
         organization_id,
         department_id,
+        division_id,
         role_id,
       },
       select: {
